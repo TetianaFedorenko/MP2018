5a4964bd4af2bcf89a4727947e053ec59252a6cf
TetianaFedorenko <fedorenko.tet@gmail.com>
2019-03-05 17:26:22 +0200

    Added HW2

--- src/main/java/hw2/HW2.java
+++ src/main/java/hw2/HW2.java
@@ -2,11 +2,15 @@ package hw2;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Created by Tetiana on 18-Dec-18.
  */
 public class HW2 {
+
+    Scanner scanner = new Scanner(System.in);
+
     int[] arr = new int[10];
     int max = 10;
     int min = -10;
@@ -54,6 +58,96 @@ public class HW2 {
             System.out.println("Number " + num + " doesn't belong to the array");
         }
 
+    }
+
+    /*Task 4: Write a program called CheckOddEven which prints "Odd Number" if the int variable “number” is odd,
+    or “Even Number” otherwise.*/
+    public void checkOddEven() {
+        System.out.println("Please enter some number");
+        double a = scanner.nextDouble();
+        if (a%2 == 0 && a != 0){
+            System.out.println("Even Number");
+        } else if (a%2 != 0) {
+            System.out.println("Odd Number");
+        } else if (a == 0) {
+            System.out.println("You entered zero. Try again");
+            scanner.next();
+        }
+    }
+
+    /*Task 5: Write a program called PrintNumberInWord which prints "ONE", "TWO",... , "NINE", "OTHER" if
+    the int variable "number" is 1, 2,... , 9, or other, respectively.*/
+    public void printNumberInWord() {
+        System.out.println("Please enter some number");
+        int a = scanner.nextInt();
+        switch (a) {
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+            case 7:
+                System.out.println("SEVEN");
+                break;
+            case 8:
+                System.out.println("EIGHT");
+                break;
+            case 9:
+                System.out.println("NINE");
+                break;
+            default:
+                System.out.println("OTHER");
+                break;
+        }
+    }
+
+    /*Task6: Write a program called SumAndAverage to produce the sum of 1, 2, 3, ..., to 100. Also compute and display the average.*/
+
+    public void sumAndAverage() {
+        int i;
+        int sum = 0;
+        for (i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println("Sum is: " + sum);
+        System.out.println("Avarage is: " + sum/2);
+    }
+
+    /*Task7: Write a program called ComputePI to compute the value of π (compare it with Math.PI)*/
+    public void computePI() {
+        double p, c, d;
+        System.out.println("Enter length");
+        c = scanner.nextDouble();
+        System.out.println("Enter diameter");
+        d = scanner.nextDouble();
+        p = c/d;
+        System.out.println("PI is: " + p);
+        System.out.println("PI is: " + Math.PI);
+    }
+
+    /*Task8: Write a program called HarmonicSum to compute the sum of a harmonic series, as shown below, where n=50000*/
+    public void harmonicSum() {
+        int harmonic = 50000;
+        int i = 1;
+        double sum = 0;
+        while (i <= harmonic) {
+            sum = sum + (1/(double)i);
+            i++;
+        }
+        System.out.println("Result is: " + sum);
     }
 
     private void generateIntegerArray(int[] arr) {
--- src/main/java/hw2/HW2Runner.java
+++ src/main/java/hw2/HW2Runner.java
@@ -1,16 +1,43 @@
 package hw2;
 
+import java.util.Scanner;
+
 /**
  * Created by Tetiana on 18-Dec-18.
  */
 public class HW2Runner {
     public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
         HW2 hw2 = new HW2();
-        System.out.println("Task 1: Write a Java program by loops to print the pattern");
-        hw2.printThePatternByLoop();
-        System.out.println("\nTask 2: Sort an integer array of 10 elements in ascending order (using the bubble sort algorithm)");
-        hw2.bubbleSortInAscendingOrder();
-        System.out.println("\nTask 3: Find if number belongs to the array using binary search algorithm\n");
-        hw2.findElementUsingBinarySearchAlgorithm();
+        System.out.println("Enter the task number (1-8)");
+        int tn = scanner.nextInt();
+        switch (tn) {
+            case 1: System.out.println("Task 1: Write a Java program by loops to print the pattern");
+                    hw2.printThePatternByLoop();
+                    break;
+            case 2: System.out.println("\nTask 2: Sort an integer array of 10 elements in ascending order (using the bubble sort algorithm)");
+                    hw2.bubbleSortInAscendingOrder();
+                    break;
+            case 3: System.out.println("\nTask 3: Find if number belongs to the array using binary search algorithm\n");
+                    hw2.findElementUsingBinarySearchAlgorithm();
+                break;
+            case 4: System.out.println("\nTask 4: Write a program which prints \"Odd Number\" if the int variable “number” is odd, " +
+                    "or “Even Number” otherwise.\n");
+                    hw2.checkOddEven();
+                    break;
+            case 5: System.out.println("\nTask 5: Write a program which prints \"ONE\", \"TWO\",... , \"NINE\", \"OTHER\" if " +
+                    "the int variable \"number\" is 1, 2,... , 9, or other, respectively.");
+                    hw2.printNumberInWord();
+                    break;
+            case 6: System.out.println("\nTask 6: Write a program to produce the sum of 1, 2, 3, ..., to 100. Also compute and display the average.");
+                    hw2.sumAndAverage();
+                    break;
+            case 7: System.out.println("\nTask 7: Write a program called ComputePI to compute the value of π (compare it with Math.PI)");
+                    hw2.computePI();
+                    break;
+            case 8: System.out.println("\nTask 8: Write a program called HarmonicSum to compute the sum of a harmonic series, as shown below, where n=50000");
+                    hw2.harmonicSum();
+                    break;
+        }
     }
 }
